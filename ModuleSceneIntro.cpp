3801bdb13ecddc0c0cce879dece56afd6b0e1b0d
TheGewehr <alexandre07979@gmail.com>
2021-10-25 19:30:47 +0200

    Making power collisions

--- ModuleSceneIntro.cpp
+++ ModuleSceneIntro.cpp
@@ -51,6 +51,449 @@ bool ModuleSceneIntro::Start()
 	ricochet01->id = 3;
 	ricochet01->body->SetType(b2_staticBody);
 
+
+	b2Vec2 map_[50] = {
+	{344, 5	  },
+	{279, 17  },
+	{213, 49  },
+	{197, 64  },
+	{81, 181  },
+	{46, 217  },
+	{12, 280  },
+	{8, 302	  },
+	{8, 419	  },
+	{24, 426  },
+	{24, 445  },
+	{16, 451  },
+	{15, 644  },
+	{47, 676  },
+	{47, 696  },
+	{8, 735	  },
+	{8, 897	  },
+	{18, 908  },
+	{180, 986 },
+	{193, 999 },
+	{193, 1088},
+	{399, 1088},
+	{399, 1003},
+	{410, 989 },
+	{575, 906 },
+	{588, 893 },
+	{588, 721 },
+	{526, 659 },
+	{526, 598 },
+	{543, 574 },
+	{589, 485 },
+	{589, 389 },
+	{573, 372 },
+	{524, 186 },
+	{522, 157 },
+	{489, 154 },
+	{487, 102 },
+	{503, 104 },
+	{538, 133 },
+	{574, 188 },
+	{596, 268 },
+	{595, 975 },
+	{636, 975 },
+	{635, 252 },
+	{621, 182 },
+	{598, 138 },
+	{571, 104 },
+	{515, 49  },
+	{454, 19  },
+	{384, 5	  }
+	};
+
+	b2Vec2 Obj01[5] = {
+	{93, 723  },
+	{108, 722 },
+	{155, 836 },
+	{153, 848 },
+	{92, 831  }
+	};
+
+	b2Vec2 Obj02[5] = {
+	{486, 724 },
+	{439, 838 },
+	{447, 848 },
+	{502, 831 },
+	{503, 724 }
+	};
+
+	b2Vec2 Obj03[6] = {
+	{51, 748   },
+	{55, 864   },
+	{174, 907  },
+	{162, 926  },
+	{53, 873   },
+	{45, 861   }
+	};
+
+	b2Vec2 Obj04[7] = {
+	{546, 746},
+	{540, 862},
+	{530, 869},
+	{420, 906},
+	{434, 928},
+	{542, 873},
+	{551, 862}
+	};
+
+	b2Vec2 Obj05[8] = {
+	{293, 118  },
+	{307, 118  },
+	{315, 125  },
+	{315, 166  },
+	{307, 173  },
+	{294, 173  },
+	{284, 166  },
+	{284, 125  }
+	};
+
+	b2Vec2 Obj06[8] = {
+	{363, 128},
+	{374, 128},
+	{383, 135},
+	{383, 175},
+	{374, 182},
+	{363, 182},
+	{352, 175},
+	{352, 135}
+	};
+
+	b2Vec2 Obj07[8] = {
+	{430, 118 },
+	{443, 118 },
+	{451, 126 },
+	{451, 165 },
+	{444, 172 },
+	{430, 172 },
+	{420, 166 },
+	{420, 126 }
+	};
+
+	b2Vec2 Obj08[27] = {
+	{226, 103  },
+	{90, 240   },
+	{68, 272   },
+	{57, 304   },
+	{60, 322   },
+	{121, 445  },
+	{126, 441  },
+	{67, 324   },
+	{70, 306   },
+	{89, 297   },
+	{105, 302  },
+	{154, 401  },
+	{159, 396  },
+	{101, 282  },
+	{92, 279   },
+	{91, 251   },
+	{102, 233  },
+	{114, 240  },
+	{187, 166  },
+	{181, 155  },
+	{193, 141  },
+	{205, 142  },
+	{229, 153  },
+	{242, 153  },
+	{248, 146  },
+	{248, 105  },
+	{242, 102  }
+	};
+
+	b2Vec2 Obj09[4] = {
+	{532, 457 },
+	{491, 535 },
+	{509, 543 },
+	{550, 462 }
+	};
+
+	//b2Vec2 Obj07
+
+	// Creation of the map ///////////////////////////////////////////////////////////////////////////////
+	// main static object
+
+	//b2BodyDef bd;
+	//ground = world->CreateBody(&bd);
+
+	int x = 0;
+	int y = 0;
+
+	b2BodyDef body;
+	body.type = b2_staticBody;
+	body.position.Set(PIXEL_TO_METERS(x), PIXEL_TO_METERS(y));
+
+	b2Body* map = App->physics->AddToWorld(&body);
+	//world->CreateBody(&body)
+
+	b2ChainShape shape;
+	b2Vec2 wVertices[50];
+
+	for (int i = 0; i < 50; i++)
+	{
+		wVertices[i] = PIXEL_TO_METERS(map_[i]);
+	}
+
+	shape.CreateChain(wVertices, 50);
+
+
+	b2FixtureDef fixture;
+	fixture.shape = &shape;
+	map->CreateFixture(&fixture);
+
+	// Static intern objects
+	/// 1
+
+	b2BodyDef intern01;
+	Object01 = App->physics->AddToWorld(&intern01);
+
+	b2BodyDef body01;
+	body01.type = b2_staticBody;
+	body01.position.Set(PIXEL_TO_METERS(x), PIXEL_TO_METERS(y));
+
+	b2Body* obj01 = App->physics->AddToWorld(&body01);
+
+	b2ChainShape shape01;
+	b2Vec2 Vertices01[5];
+
+	for (int i = 0; i < 5; i++)
+	{
+		Vertices01[i] = PIXEL_TO_METERS(Obj01[i]);
+	}
+
+	shape01.CreateChain(Vertices01, 5);
+
+
+	b2FixtureDef fixture01;
+	fixture01.shape = &shape01;
+	obj01->CreateFixture(&fixture01);
+	///
+
+	// Static intern objects
+	/// 2
+
+	b2BodyDef intern02;
+	Object02 = App->physics->AddToWorld(&intern02);
+
+
+	b2BodyDef body02;
+	body02.type = b2_staticBody;
+	body02.position.Set(PIXEL_TO_METERS(x), PIXEL_TO_METERS(y));
+
+	b2Body* obj02 = App->physics->AddToWorld(&body02);
+
+	b2ChainShape shape02;
+	b2Vec2 Vertices02[5];
+
+	for (int i = 0; i < 5; i++)
+	{
+		Vertices02[i] = PIXEL_TO_METERS(Obj02[i]); ///
+	}
+
+	shape02.CreateChain(Vertices02, 5);
+
+	b2FixtureDef fixture02;
+	fixture02.shape = &shape02;
+	obj02->CreateFixture(&fixture02);
+	///
+
+	// Static intern objects
+	/// 3
+
+	b2BodyDef intern03;
+	Object02 = App->physics->AddToWorld(&intern03);
+
+
+	b2BodyDef body03;
+	body03.type = b2_staticBody;
+	body03.position.Set(PIXEL_TO_METERS(x), PIXEL_TO_METERS(y));
+
+	b2Body* obj03 = App->physics->AddToWorld(&body03);
+
+	b2ChainShape shape03;
+	b2Vec2 Vertices03[6];
+
+	for (int i = 0; i < 6; i++)
+	{
+		Vertices03[i] = PIXEL_TO_METERS(Obj03[i]); ///
+	}
+
+	shape03.CreateChain(Vertices03, 6);
+
+	b2FixtureDef fixture03;
+	fixture03.shape = &shape03;
+	obj03->CreateFixture(&fixture03);
+	///
+
+	// Static intern objects
+	/// 4
+
+	b2BodyDef intern04;
+	Object04 = App->physics->AddToWorld(&intern04);
+
+
+	b2BodyDef body04;
+	body04.type = b2_staticBody;
+	body04.position.Set(PIXEL_TO_METERS(x), PIXEL_TO_METERS(y));
+
+	b2Body* obj04 = App->physics->AddToWorld(&body04);
+
+	b2ChainShape shape04;
+	b2Vec2 Vertices04[7];
+
+	for (int i = 0; i < 7; i++)
+	{
+		Vertices04[i] = PIXEL_TO_METERS(Obj04[i]); ///
+	}
+
+	shape04.CreateChain(Vertices04, 7);
+
+	b2FixtureDef fixture04;
+	fixture04.shape = &shape04;
+	obj04->CreateFixture(&fixture04);
+	///
+
+	// Static intern objects
+	/// 5
+
+	b2BodyDef intern05;
+	Object05 = App->physics->AddToWorld(&intern05);
+
+
+	b2BodyDef body05;
+	body05.type = b2_staticBody;
+	body05.position.Set(PIXEL_TO_METERS(x), PIXEL_TO_METERS(y));
+
+	b2Body* obj05 = App->physics->AddToWorld(&body05);
+
+	b2ChainShape shape05;
+	b2Vec2 Vertices05[8];
+
+	for (int i = 0; i < 8; i++)
+	{
+		Vertices05[i] = PIXEL_TO_METERS(Obj05[i]); ///
+	}
+
+	shape05.CreateChain(Vertices05, 8);
+
+	b2FixtureDef fixture05;
+	fixture05.shape = &shape05;
+	obj05->CreateFixture(&fixture05);
+	///
+
+	// Static intern objects
+	/// 6
+
+	b2BodyDef intern06;
+	Object06 = App->physics->AddToWorld(&intern06);
+
+
+	b2BodyDef body06;
+	body06.type = b2_staticBody;
+	body06.position.Set(PIXEL_TO_METERS(x), PIXEL_TO_METERS(y));
+
+	b2Body* obj06 = App->physics->AddToWorld(&body06);
+
+	b2ChainShape shape06;
+	b2Vec2 Vertices06[8];
+
+	for (int i = 0; i < 8; i++)
+	{
+		Vertices06[i] = PIXEL_TO_METERS(Obj06[i]); ///
+	}
+
+	shape06.CreateChain(Vertices06, 8);
+
+	b2FixtureDef fixture06;
+	fixture06.shape = &shape06;
+	obj06->CreateFixture(&fixture06);
+	///
+
+	// Static intern objects
+	/// 7
+
+	b2BodyDef intern07;
+	Object07 = App->physics->AddToWorld(&intern07);
+
+
+	b2BodyDef body07;
+	body07.type = b2_staticBody;
+	body07.position.Set(PIXEL_TO_METERS(x), PIXEL_TO_METERS(y));
+
+	b2Body* obj07 = App->physics->AddToWorld(&body07);
+
+	b2ChainShape shape07;
+	b2Vec2 Vertices07[8];
+
+	for (int i = 0; i < 8; i++)
+	{
+		Vertices07[i] = PIXEL_TO_METERS(Obj07[i]); ///
+	}
+
+	shape07.CreateChain(Vertices07, 8);
+
+	b2FixtureDef fixture07;
+	fixture07.shape = &shape07;
+	obj07->CreateFixture(&fixture07);
+	///
+
+	// Static intern objects
+	/// 8
+
+	b2BodyDef intern08;
+	Object08 = App->physics->AddToWorld(&intern08);
+
+
+	b2BodyDef body08;
+	body08.type = b2_staticBody;
+	body08.position.Set(PIXEL_TO_METERS(x), PIXEL_TO_METERS(y));
+
+	b2Body* obj08 = App->physics->AddToWorld(&body08);
+
+	b2ChainShape shape08;
+	b2Vec2 Vertices08[27];
+
+	for (int i = 0; i < 27; i++)
+	{
+		Vertices08[i] = PIXEL_TO_METERS(Obj08[i]); ///
+	}
+
+	shape08.CreateChain(Vertices08, 27);
+
+	b2FixtureDef fixture08;
+	fixture08.shape = &shape08;
+	obj08->CreateFixture(&fixture08);
+	///
+
+	// Static intern object
+	// 9
+	b2BodyDef intern09;
+	Object01 = App->physics->AddToWorld(&intern09);
+
+	b2BodyDef body09;
+	body09.type = b2_staticBody;
+	body09.position.Set(PIXEL_TO_METERS(x), PIXEL_TO_METERS(y));
+
+	b2Body* obj09 = App->physics->AddToWorld(&body09);
+
+	b2ChainShape shape09;
+	b2Vec2 Vertices09[4];
+
+	for (int i = 0; i < 4; i++)
+	{
+		Vertices09[i] = PIXEL_TO_METERS(Obj09[i]);
+	}
+
+	shape09.CreateChain(Vertices09, 4);
+
+
+	b2FixtureDef fixture09;
+	fixture09.shape = &shape09;
+	obj09->CreateFixture(&fixture09);
+
+
 	return ret;
 }
 
@@ -223,11 +666,13 @@ void ModuleSceneIntro::OnCollision(PhysBody* bodyA, PhysBody* bodyB)
 		}
 		if ((bodyA->id == 0) && (bodyB->id == 3))
 		{
-			App->audio->PlayFx(bonus_fx);
+			//App->audio->PlayFx(bonus_fx);
+			int multi = 20;
 			b2Vec2 direction;
-			direction = (bodyA->body->GetLocalCenter()) - (bodyB->body->GetLocalCenter()) ; // World or local
-			direction = { direction.x * 1,direction.y * 1 };
-			bodyA->body->ApplyLinearImpulse(direction, bodyA->body->GetWorldCenter(), true);
+			direction = (bodyA->body->GetWorldCenter()) - (bodyB->body->GetWorldCenter()) ; // World or local
+			direction = { direction.x * multi,direction.y * multi };
+			bodyA->body->SetLinearVelocity(direction);
+			App->audio->PlayFx(bonus_fx);
 			//ApplyForce(direction, bodyA->body->GetWorldCenter(), true);
 
 			//TODO sum points
